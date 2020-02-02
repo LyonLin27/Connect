70efcd254b8986699e20a63fb1f5e41f5befe863
TonyWang98 <wqtls1998@gmail.com>
2020-02-01 22:14:32 -0500

    Enemy & Enemy Projectile

--- Assets/Scripts/GameMan.cs
+++ Assets/Scripts/GameMan.cs
@@ -4,9 +4,62 @@ using UnityEngine;
 
 public class GameMan : MonoBehaviour
 {
+    public static GameMan Instance { get; private set; }
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+
     public GameObject PlayerAgent;
+    public GameObject projectileNormalPrefab;
+    public List<GameObject> projectileNormal;
+    public int projectileNomralIndex = -1;
+
+    public GameObject testObj;
+    public GameObject testObj2;
+
 
     private void Awake() {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject projectileObj  = Instantiate(projectileNormalPrefab);
+            projectileObj.SetActive(false);
+            projectileNormal.Add(projectileObj);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            testObj.GetComponent<Enemy>().StartFire();
+            testObj2.GetComponent<Enemy>().StartFire();
+        }
+    }
+
+    public int GetEnemyProjectile(int projectileType)
+    {
+        switch (projectileType)
+        {
+            case 0: //Normal Projectile
+                projectileNomralIndex++;
+                return projectileNomralIndex;
+            case 1:
+                return 0;
+        }
+        return -1;
+    }
+
 }
