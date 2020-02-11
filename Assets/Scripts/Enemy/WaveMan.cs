35af1b538233b6f962a4c0d5a6091bc268738cbe
linh8 <linh8@rpi.edu>
2020-02-11 00:11:59 -0500

    wave man

---
+++ Assets/Scripts/Enemy/WaveMan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMan : MonoBehaviour
+{
+    public int startWave = 0;
+    public int currWave = 0;
+    private List<Enemy> enemyList;
+    private bool waveEnd;
+
+    private void Start() {
+        currWave = startWave;
+    }
+    private void Update() {
+        if (waveEnd && enemyList.Count == 0) {
+            waveEnd = false;
+            currWave++;
+            StartCoroutine("Wave" + currWave);
+        }
+    }
+
+    IEnumerator Wave1() {
+        yield return new WaitForSeconds(2f);
+
+
+    }
+}
